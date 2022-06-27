1464abf999016eb49ae0a340c3f50cbb2217a5eb
Aexer <yc5693410@gmail.com>
2022-06-27 23:53:22 +0200

    navbar + hero section fixed

--- components/navbar.php
+++ components/navbar.php
@@ -1,22 +1,22 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger position-fixed">
     <div class="container-fluid m-0 p-0">
-      <div class="nav-logo-container">
+      <div class="nav-logo-container p-0 ps-sm-5">
         <img class="navbar-logo" src="./img/Leyweg_logo.png" alt="Logo">
         <a class="navbar-brand" href="index.php">Eethuis Chef</a>
       </div>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-toggle" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler ms-2 me-sm-5" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-toggle" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse bg-danger" id="navbar-toggle">
         <ul class="navbar-nav ms-auto text-center">
-          <li class="nav-item mx-2">
+          <li class="nav-item mx-2 my-2 my-md-none">
             <a class="nav-link active" href="menu.php">Menu</a>
           </li>
-          <li class="nav-item mx-2">
+          <li class="nav-item mx-2 my-2 my-md-none">
             <a class="nav-link active" href="aboutus.php">Over ons</a>
           </li>
-          <li class="nav-item mx-2">
+          <li class="nav-item mx-2 my-2 my-md-none">
             <a class="nav-link active" href="contact.php">Contact</a>
           </li>
         </ul>
--- index.php
+++ index.php
@@ -10,25 +10,41 @@
   <main>
     <section class="hero-section position-relative">
       <div class="hero-padding-bg"></div>
-      <div class="hero-content text-center">
-        <h1>Welkom bij Eethuis Chef</h1>
-        <br>
-        <h4>
-          Lekkerste eten van Leyweg
-        </h4>
-        <hr>
+      <div class="hero-content">
+        <div class="hero-title mx-auto text-center position-relative">
+          <h1>Welkom bij Eethuis Chef</h1>
+          <br>
+          <h4>
+            Lekkerste eten van Leyweg
+          </h4>
+        </div>
         <div class="card-container d-flex flex-column flex-md-row justify-content-center">
-          <div class="card text-black mx-lg-5 mx-auto mt-5 px-5 py-3 d-flex">
-            <div class="review text-start">Geweldige burgers</div>
-            <div class="reviewer text-end">-Yusa</div>
+          <div class="card text-black mx-lg-5 mx-auto my-3 m-md-0 px-5 py-3 d-flex flex-column justify-content-between">
+            <div class="review text-center">Lorem ipsum dolor sit amet consectetur adipisicing.</div>
+            <div class="rating text-end d-flex justify-content-around">
+              <div class="stars">⭐⭐⭐⭐⭐</div>
+              <div class="reviewer">
+                -Yusa
+              </div>
+            </div>
           </div>
-          <div class="card text-black mx-lg-5 mx-auto mt-5 px-5 py-3 d-flex">
-            <div class="review text-start">Geweldige burgers</div>
-            <div class="reviewer text-end">-Yusa</div>
+          <div class="card text-black mx-lg-5 mx-auto my-3 m-md-0 px-5 py-3 d-flex flex-column justify-content-between">
+            <div class="review text-center">Lorem ipsum dolor sit amet consectetur.</div>
+            <div class="rating text-end d-flex justify-content-around">
+              <div class="stars">⭐⭐⭐⭐⭐</div>
+              <div class="reviewer">
+                -Muhammed
+              </div>
+            </div>
           </div>
-          <div class="card text-black mx-lg-5 mx-auto mt-5 px-5 py-3 d-flex">
-            <div class="review text-start">Geweldige burgers</div>
-            <div class="reviewer text-end">-Yusa</div>
+          <div class="card text-black mx-lg-5 mx-auto my-3 m-md-0 px-5 py-3 d-flex flex-column justify-content-between">
+            <div class="review text-center">Lorem ipsum dolor sit amet consectetur adipisicing.</div>
+            <div class="rating text-end d-flex justify-content-around">
+              <div class="stars">⭐⭐⭐⭐⭐</div>
+              <div class="reviewer">
+                -Yusuf
+              </div>
+            </div>
           </div>
         </div>
       </div>
